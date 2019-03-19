16387479c4bdcdaa31b7aea5d8c3422b29e430ae
Marise Hayashi <limitedeternity@users.noreply.github.com>
2019-03-20 00:45:36 +0300

    Update make_dscage.rb

--- ctl/cmd/make_dscage.rb
+++ ctl/cmd/make_dscage.rb
@@ -1,4 +1,4 @@
 def cmd_make_dscage(options)
-    sys("mkdir -p \"#{PREFIX_PATH}\"")
-    sys("chmod 777 \"#{PREFIX_PATH}\"")
+    sys("sudo mkdir -p \"#{PREFIX_PATH}\"")
+    sys("sudo chmod 777 \"#{PREFIX_PATH}\"")
 end
